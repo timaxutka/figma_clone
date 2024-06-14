b4ab64c55c4351958e63a3aaa84ec797a73de8c4
Maxim TImofeev <Elevenfive1@gmail.com>
2024-06-14 19:05:25 +0600

    change logo

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -9,7 +9,7 @@ const Loader = () => (
       height={100}
       className='object-contain'
     />
-    <p className='text-sm font-bold text-primary-grey-300'>Loading...</p>
+    <p className='text-sm font-bold text-primary-grey-300'>Загрузка...</p>
   </div>
 );
 
